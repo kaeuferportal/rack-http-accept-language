cf43826d3ea0d06e0c0348a18c7bebe923e4db29
Garllon <palluthe.bennet@gmail.com>
2016-03-24 18:12:20 +0100

    handle wildcard languages and no languages

--- lib/rack-http-accept-language/parser.rb
+++ lib/rack-http-accept-language/parser.rb
@@ -1,28 +1,39 @@
 # frozen_string_literal: true
 module RackHttpAcceptLanguage
   class Parser
+    attr_reader :http_accept_language
+
     def initialize(http_accept_language)
       @http_accept_language = http_accept_language
     end
 
     def preferred_language
-      preferred_languages.first
+      return nil if http_accept_language.nil?
+      order_languages_by_qvalue.first
     end
 
     def preferred_languages
-      order_languages_by_qvalue.values.flatten
+      return nil if http_accept_language.nil?
+      order_languages_by_qvalue
     end
 
     private
 
-    def split_languages
-      @split_languages ||= @http_accept_language.split(',')
+    def order_languages_by_qvalue
+      order_languages_by_qvalue_hash.values.flatten
     end
 
-    def order_languages_by_qvalue
+    def split_languages
+      @split_languages ||= http_accept_language.split(',')
+    end
+
+    def order_languages_by_qvalue_hash
       @order_languages_by_qvalue ||=
         split_languages.each_with_object({}) do |language_with_qvalue, memo|
           language, qvalue = language_with_qvalue.split(';q=')
+          next if language == '*'
+
+          language = language.downcase.gsub(/-[a-z0-9]+$/i, &:upcase)
           qvalue = qvalue ? qvalue.to_f : 1.0
 
           if memo[qvalue].nil?
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,17 +3,57 @@ require 'spec_helper'
 
 describe RackHttpAcceptLanguage::Parser do
   context 'without wildcard' do
-    let(:http_accept_language) { 'en-US,en-GB;q=0.6,en;q=0.8' }
-    subject(:parser) { described_class.new(http_accept_language) }
+    context 'no upcase handling necessary' do
+      let(:http_accept_language) { 'en-US,en-GB;q=0.6,en;q=0.8' }
+      subject(:parser) { described_class.new(http_accept_language) }
 
-    it 'preferred language' do
-      expected_array = 'en-US'
-      expect(parser.preferred_language).to eq expected_array
+      it 'preferred language' do
+        expected_array = 'en-US'
+        expect(parser.preferred_language).to eq expected_array
+      end
+
+      it 'prefrerred_languages' do
+        expected_array = ['en-US', 'en', 'en-GB']
+        expect(parser.preferred_languages).to eq expected_array
+      end
     end
 
-    it 'prefrerred_languages' do
-      expected_array = ['en-US', 'en', 'en-GB']
-      expect(parser.preferred_languages).to eq expected_array
+    context 'upcase necessary' do
+      let(:http_accept_language) { 'en-us,en-GB;q=0.6,en;q=0.8' }
+      subject(:parser) { described_class.new(http_accept_language) }
+
+      it 'preferred language' do
+        expected_array = 'en-US'
+        expect(parser.preferred_language).to eq expected_array
+      end
+
+      it 'prefrerred_languages' do
+        expected_array = ['en-US', 'en', 'en-GB']
+        expect(parser.preferred_languages).to eq expected_array
+      end
+    end
+  end
+
+  context 'with wildcard' do
+    it 'has no http_accept_language' do
+      http_accept_language = nil
+      parser = described_class.new(http_accept_language)
+
+      expect(parser.preferred_language).to eq nil
+    end
+
+    it 'has a * as http_accept_language' do
+      http_accept_language = '*'
+      parser = described_class.new(http_accept_language)
+
+      expect(parser.preferred_language).to eq nil
+    end
+
+    it 'has a * in http_accept_language' do
+      http_accept_language = '*,en;q=0.6'
+      parser = described_class.new(http_accept_language)
+
+      expect(parser.preferred_language).to eq 'en'
     end
   end
 end
